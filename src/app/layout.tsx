74c65de5ee82e644d870869c3b7ce5eab55da76b
TaarnN <157127790+TaarnN@users.noreply.github.com>
2024-09-25 17:29:01 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "The Stary Night Lantern",
-  description: "KUS STEM3 P.6/6 group 4 - The Stary Night Lantern",
+  title: "The Harvest Lantern",
+  description: "KUS STEM3 P.6/6 group 4 - The Harvest Lantern",
 };
 
 export default function RootLayout({
